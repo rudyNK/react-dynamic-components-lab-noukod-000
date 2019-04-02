a80cf231a81b903418c38490d6aa99feabc2f2b0
Rudy Ravelin <rudy@noukod.com>
2019-04-02 16:56:12 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,12 +1,12 @@
 Skip to content
- 
+
 Search or jump to…
 
 Pull requests
 Issues
 Marketplace
 Explore
- 
+
 @rudyNK Sign out
 0
 0 3 kodyclemens/react-dynamic-components-lab-online-web-ft-102218
@@ -17,20 +17,20 @@ react-dynamic-components-lab-online-web-ft-102218/src/BlogPost.js
 b3f80dc 27 days ago
 @DanielSeehausen @kodyclemens
 30 lines (26 sloc)  1.46 KB
-  
+
 import React, { Component } from 'react';
 import Comment from './Comment';
 // are we missing an import?
 
 export default class BlogPost extends Component {
   render() {
-    
+
     const comments = [
-      "Tell my wifi love her", 
+      "Tell my wifi love her",
       "Ken M was here",
       "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
     ]
-    
+
     return (
       <div id="blog-post" className="wrapper">
         Alright internet trolls<br/>
